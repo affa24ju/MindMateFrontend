d6f3def8a73dfd6aabe931baf5be8e97abc5d461
Fatema Afroza <fafroza2017@gmail.com>
2025-10-14 14:14:44 +0200

    La till delete-entry inne i journal-history

---
+++ src/app/components/delet-entry/delet-entry.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delet-entry',
+  imports: [],
+  templateUrl: './delet-entry.html',
+  styleUrl: './delet-entry.css'
+})
+export class DeletEntry {
+
+}
--- src/app/components/journal-history/journal-history.ts
+++ src/app/components/journal-history/journal-history.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { JournalEntry, JournlService  } from '../../services/journl-service';
 import { FeelingService } from '../../services/feeling-service';
+import { DeletEntry } from '../delet-entry/delet-entry';
 
 @Component({
   selector: 'app-journal-history',
-  imports: [CommonModule],
+  imports: [CommonModule, DeletEntry],
   templateUrl: './journal-history.html',
   styleUrl: './journal-history.css'
 })
@@ -35,6 +36,10 @@ export class JournalHistory implements OnInit {
   // Emojies
   getEmoji(feeling: any){
     return this.feelingService.getEmoji(feeling);
+  }
+
+  reloadAfterChange() {
+    this.loadAllEntries();
   }
   
 }
