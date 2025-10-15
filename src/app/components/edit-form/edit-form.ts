696e2f50ee70ec74f547462fff95fefdc814d5d0
Fatema Afroza <fafroza2017@gmail.com>
2025-10-15 07:29:25 +0200

    La till startEdit() & cancel() metod inne i ts fil

--- src/app/components/edit-form/edit-form.ts
+++ src/app/components/edit-form/edit-form.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { JournlService } from '../../services/journl-service';
 
 @Component({
   selector: 'app-edit-form',
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './edit-form.css'
 })
 export class EditForm {
+  @Input() entry!: any;
+  @Output() updated = new EventEmitter<void>();
+
+  editing = false;
+  note = '';
+
+  // Injecerar JournalService 
+  constructor(private journalService: JournlService) {}
+
+  startEdit() {
+    this.editing = true;
+    this.note = this.entry.note;
+  }
+
+  cancet() {
+    this.editing = false;
+  }
+
 
 }
