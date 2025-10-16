c038745ae49ef2261df6220d8f264ce36adb18be
Fatema Afroza <fafroza2017@gmail.com>
2025-10-16 21:40:34 +0200

    La till sendMetod som ska anropa backend via aiService

--- src/app/components/ai-chat/ai-chat.ts
+++ src/app/components/ai-chat/ai-chat.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AiService } from '../../services/ai-service';
 
 @Component({
   selector: 'app-ai-chat',
@@ -16,8 +17,30 @@ export class AiChat {
   aiResponse = '';
   loading = false;
 
+  // Constructor för att injecera aiService
+  constructor(private aiService: AiService) {}
+
   // Metod för att skicka frågan
   sendMessage() {
+    console.log('Klick på send knapp i Ai-chat');
+    
+    if (!this.userMessage.trim()) return;
+
+    this.loading = true;
+    this.aiResponse = '';
+
+    // Anropar backend via AiService
+    this.aiService.getRecipeSuggestion(this.userMessage).subscribe({
+      next: (response) => {
+        this.aiResponse = response;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.aiResponse = 'Något gick fel. Försök igen lite senare!';
+        this.loading = false;
+        console.error(err);      
+      }
+    });
     
   }
 
@@ -26,5 +49,4 @@ export class AiChat {
     console.log('Klick på X kanpp');
     this.closed.emit();
   }
-
 }
