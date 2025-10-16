d61f6ba56a0052d69a598e05d9c876e19e44c81f
Fatema Afroza <fafroza2017@gmail.com>
2025-10-16 20:40:48 +0200

    La till text area & knapp

--- src/app/components/ai-chat/ai-chat.ts
+++ src/app/components/ai-chat/ai-chat.ts
@@ -1,14 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ai-chat',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './ai-chat.html',
   styleUrl: './ai-chat.css'
 })
 export class AiChat {
 
   @Output() closed = new EventEmitter<void>();
+
+  userMessage = '';
+  aiResponse = '';
+  loading = false;
+
+  // Metod för att skicka frågan
+  sendMessage() {
+    
+  }
 
   // Metod för att stänga fönster
   close() {
