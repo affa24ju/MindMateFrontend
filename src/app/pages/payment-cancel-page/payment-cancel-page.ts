c11465d2c6cfc7b104a65cce940851ac8a734f02
Fatema Afroza <fafroza2017@gmail.com>
2025-10-21 13:28:16 +0200

    La till knapp & router till journal-page

--- src/app/pages/payment-cancel-page/payment-cancel-page.ts
+++ src/app/pages/payment-cancel-page/payment-cancel-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-payment-cancel-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './payment-cancel-page.html'
   
 })
