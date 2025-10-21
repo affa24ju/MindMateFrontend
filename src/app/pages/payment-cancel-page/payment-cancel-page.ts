6df5814b189e86dbb0ec28e9bfbfe245d50fe2db
Fatema Afroza <fafroza2017@gmail.com>
2025-10-21 13:10:07 +0200

    Skapat payment-cancel komponet

---
+++ src/app/pages/payment-cancel-page/payment-cancel-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-cancel-page',
+  imports: [],
+  templateUrl: './payment-cancel-page.html'
+  
+})
+export class PaymentCancelPage {
+
+}
