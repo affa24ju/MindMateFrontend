29cbe5d1c9ed42513ba003d138fa5f41ed89cd8b
Fatema Afroza <fafroza2017@gmail.com>
2025-10-24 20:58:47 +0200

    Skapat user-service med getPremiumStatus()

---
+++ src/app/services/user-service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  // Bas-URL för backend API
+  private apiUrl = 'http://localhost:8080/api/myJournal/users';
+
+  // Constructor
+  constructor(private http: HttpClient) { }
+
+  // Metod för att hämta premium status för användaren
+  getPremiumStatus(): Observable<{ premium: boolean }> {
+    return this.http.get<{ premium: boolean }>(`${this.apiUrl}/me/premium`);
+  }
+  
+}
